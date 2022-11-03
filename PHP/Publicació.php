5f5f5ff4743324a5d81a74e677c68a86e9626b78
JuliaK18 <juliakrukonis@iesmontsia.org>
2022-11-03 20:03:27 +0100

    Funcionalitats logout i Publicacio

--- PHP/Publicació.php
+++ PHP/Publicació.php
@@ -1,8 +1,30 @@
 <?php
-//id
-//dataPublicacio
-//censurat
-//idPost
-//idComentari
-//idUsuari
+class Publicacio{
+    private $id;
+    private $dataPublicacio;
+    private $censurat;
+    private $idPost;
+    private $idComentari;
+    private $idUsuari;
+    
+    function __construct($id, $dataPublicacio, $censurat, $idPost, $idComentari, $idUsuari) {
+        $this->id = $id;
+        $this-> dataPublicacio = $dataPublicacio;
+        $this-> censurat = $censurat;
+        $this-> idPost = $idPost;
+        $this-> idComentari = $idComentari;
+        $this-> idUsuari = $idUsuari;
+    }
 
+    function eliminarPost($id){
+        conectar();
+        $query ="DELETE * from Publicacio where id.publicacio = '$id';";
+        desconetar();
+    }
+    
+    function consultaPost($linea){
+        $resultat =$linea->fetch_assoc();
+        return $resultat[$nom];
+    }
+
+}
