e8ab677f55879ff1e8f44f33ee7072ac1981df6e
JuliaK18 <juliakrukonis@iesmontsia.org>
2022-11-03 17:55:36 +0100

    INSERTS

---
+++ Inserts/Recurs.sql
@@ -0,0 +1,10 @@
+insert into Recurs (id, TipusRecurs, Referencia, IdComentari, Ocult, IdPublicacio) values (1, '', '', '', false, 1);
+insert into Recurs (id, TipusRecurs, Referencia, IdComentari, Ocult, IdPublicacio) values (2, '', '', '', false, 2);
+insert into Recurs (id, TipusRecurs, Referencia, IdComentari, Ocult, IdPublicacio) values (3, '', '', '', true, 3);
+insert into Recurs (id, TipusRecurs, Referencia, IdComentari, Ocult, IdPublicacio) values (4, '', '', '', true, 4);
+insert into Recurs (id, TipusRecurs, Referencia, IdComentari, Ocult, IdPublicacio) values (5, '', '', '', true, 5);
+insert into Recurs (id, TipusRecurs, Referencia, IdComentari, Ocult, IdPublicacio) values (6, '', '', '', true, 6);
+insert into Recurs (id, TipusRecurs, Referencia, IdComentari, Ocult, IdPublicacio) values (7, '', '', '', false, 7);
+insert into Recurs (id, TipusRecurs, Referencia, IdComentari, Ocult, IdPublicacio) values (8, '', '', '', true, 8);
+insert into Recurs (id, TipusRecurs, Referencia, IdComentari, Ocult, IdPublicacio) values (9, '', '', '', true, 9);
+insert into Recurs (id, TipusRecurs, Referencia, IdComentari, Ocult, IdPublicacio) values (10, '', '', '', true, 10);
