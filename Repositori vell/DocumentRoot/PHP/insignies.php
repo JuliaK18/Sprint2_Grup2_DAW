bd9b44b1460c5f75c8ec24da8a1950aeb0634bb9
yasirhayati01 <yasirhacm@gmail.com>
2022-11-11 15:43:27 +0100

    insignies v2

--- Repositori vell/DocumentRoot/PHP/insignies.php
+++ Repositori vell/DocumentRoot/PHP/insignies.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
 <?php
 include 'connexio.php';
 class insignies{ 
@@ -5,13 +16,40 @@ class insignies{
      private $id;
      private $nom;
 
-        public function set_name ($id, $nom){
-                    
+        public function mostrar_Insignia ($id, $nom){
+            include_once '../connexio.php';   
+            $consulta = "SELECT * FROM Insignies WHERE id=1" ;
+            
+            if ($result = $connexioDB->query($consulta)){
+                if ($result ->num_rows > 0) {
+                    while ($obj = $result->fetch_object()) {
+                        echo "<tr>";
+                        echo "<td> $obj->Id </td>";
+                        echo "<td> $obj->Nom</td>";
+                        echo "</tr>";
+                    }
+                }
+
+            }
+
+        public function actualitzar ($id, $nom){
+            include_once '../connexio.php';
+            $consulta = "UPDATE * FROM Insignies" ;
+
+            
+
+            
+        }
+
+
+
+
             $this->id = $id;
             $this->nom = $nom;
         }
 
         function consultaInsignia($id){
+            
             $query ="select * from Insignies where id.insignica = '$id';";
             return $linea;
         }
@@ -23,4 +61,7 @@ class insignies{
 
         
 }
+?>
+</body>
+</html>
 
